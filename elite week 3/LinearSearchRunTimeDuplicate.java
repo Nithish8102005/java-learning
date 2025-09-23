0b9f3e3f452dbfa88d616f53a540c2ee353b4810
Nithish8102005 <nithiskumarm025@gmail.com>
2025-09-23 10:42:05 +0530

    change

---
+++ elite week 3/LinearSearchRunTimeDuplicate.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class LinearSearchRunTimeDuplicate{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of array: ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter the array");
+        for (int i = 0; i < n; i++) {
+           arr[i] = sc.nextInt();
+        }
+          System.out.println("Enter the key");
+          int key = sc.nextInt();
+          int position = -1;
+          for(int i=0;i<arr.length;i++){
+              if(arr[i] == key){
+                  position = i;
+                  System.out.println(" Found at index"+i);
+              }
+          }
+              if(position == -1){
+                  System.out.println("Element not Found");
+              }
+            //   else{
+            //       System.out.println("Found at index" +position);
+            //   }
+          }
+    }
