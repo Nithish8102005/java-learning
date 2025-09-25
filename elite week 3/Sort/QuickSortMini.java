79198368187a9d0e1abdbdff9a0ff33facbaa69c
Nithish8102005 <nithiskumarm025@gmail.com>
2025-09-25 11:34:11 +0530

    change

---
+++ elite week 3/Sort/QuickSortMini.java
@@ -0,0 +1,33 @@
+package Sort;
+
+public class QuickSortMini {
+
+    static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pivot = partition(arr, low, high);
+            quickSort(arr, low, pivot - 1);
+            quickSort(arr, pivot + 1, high);
+        }
+    }
+
+    static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high], i = low;
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                int temp = arr[i];
+                arr[i++] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i];
+        arr[i] = arr[high];
+        arr[high] = temp;
+        return i;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {8, 4, 7, 9, 3, 5};
+        quickSort(arr, 0, arr.length - 1);
+        for (int n : arr) System.out.print(n + " ");
+    }
+}
