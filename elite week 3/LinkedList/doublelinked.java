355308d225b55e6847ff1efa9183e678f25bff5d
Nithish8102005 <nithiskumarm025@gmail.com>
2025-09-26 15:27:41 +0530

    double linkedlist

--- elite week 3/LinkedList/doublelinked.java
+++ elite week 3/LinkedList/doublelinked.java
@@ -1,72 +1,138 @@
 package LinkedList;
 
-public class doublelinked {
-    class Node{             
+public class DoubleLinked {
+    // Node class
+    class Node {
         int data;
-        Node next;
-        Node prev;
-        Node(int value){
-            data=value;
-            next=null;
-            prev=null;
-        }                                               
-    }
-    Node head = null;
-    //Insert at end
-    public void insert(int value){
-        Node newNode = new Node(value);
-        if(head==null){
-            head=newNode;
-        }else{
-            Node current = head;
-            while(current.next!=null){
-                current=current.next;
-            }
-            current.next=newNode;
-            newNode.prev=current;
+        Node prev, next;
+
+        Node(int value) {
+            data = value;
+            prev = null;
+            next = null;
         }
-        System.out.println(value + " inserted ");
     }
-    public void insertAtBeginning(int value){
+
+    Node head = null;
+
+    // Insert at end
+    public void insertEnd(int value) {
         Node newNode = new Node(value);
-        if(head==null){
-            head=newNode;
-        }else{
-            newNode.next=head;
-            head.prev=newNode;
-            head=newNode;
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+            newNode.prev = current;
+        }
+        System.out.println(value + " inserted at end");
+    }
+
+    // Insert at beginning
+    public void insertBeginning(int value) {
+        Node newNode = new Node(value);
+        if (head == null) {
+            head = newNode;
+        } else {
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
         }
         System.out.println(value + " inserted at beginning");
     }
-    // delete by value
-    public void delete(int value){
-        if(head==null){
+
+    // Delete by value
+    public void delete(int value) {
+        if (head == null) {
             System.out.println("List is empty");
             return;
         }
-        if(head.data==value){
-            head=head.next;
-            if(head!=null){
-                head.prev=null;
+
+        Node current = head;
+
+        // If head needs to be deleted
+        if (current.data == value) {
+            head = current.next;
+            if (head != null) {
+                head.prev = null;
             }
-            System.out.println(value + " deleted ");
+            System.out.println(value + " deleted");
+            return;
+        }
+
+        // Search for node
+        while (current != null && current.data != value) {
+            current = current.next;
+        }
+
+        if (current == null) {
+            System.out.println(value + " not found in the list");
+            return;
+        }
+
+        // Unlink node
+        if (current.next != null) {
+            current.next.prev = current.prev;
+        }
+        if (current.prev != null) {
+            current.prev.next = current.next;
+        }
+        System.out.println(value + " deleted");
+    }
+
+    // Display forward
+    public void displayForward() {
+        if (head == null) {
+            System.out.println("List is empty");
             return;
         }
         Node current = head;
-        while(current!=null &&  current.data != value){
-            current=current.next;
+        System.out.print("Forward: ");
+        while (current != null) {
+            System.out.print(current.data + " <-> ");
+            current = current.next;
         }
-        if(current == null){
-            System.out.println(value + " not found in the list");
+        System.out.println("NULL");
+    }
+
+    // Display backward
+    public void displayBackward() {
+        if (head == null) {
+            System.out.println("List is empty");
+            return;
         }
-        else{
-            if(current.next!=null){
-                current.next.prev=current.prev;
-            }
-            if(current.prev!=null){
-                current.prev.next=current.next;
-            }
-            System.out.println(value + " deleted ");
+        Node current = head;
+        while (current.next != null) {
+            current = current.next; // move to last node
         }
+        System.out.print("Backward: ");
+        while (current != null) {
+            System.out.print(current.data + " <-> ");
+            current = current.prev;
+        }
+        System.out.println("NULL");
+    }
+
+    // Main method
+    public static void main(String[] args) {
+        DoubleLinked dll = new DoubleLinked();
+
+        dll.insertEnd(10);
+        dll.insertEnd(20);
+        dll.insertEnd(30);
+        dll.displayForward();
+        dll.displayBackward();
+
+        dll.insertBeginning(5);
+        dll.displayForward();
+
+        dll.delete(20);
+        dll.displayForward();
+
+        dll.delete(40); // not found
+        dll.displayForward();
     }
 }
